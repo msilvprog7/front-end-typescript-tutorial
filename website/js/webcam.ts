24cc453ef7a307b5299c03ce4be074d270f590ea
Michael Snider <msilvprog7@gmail.com>
2016-08-11 21:14:20 -0700

    View camera and capture image data.

--- website/js/webcam.ts
+++ website/js/webcam.ts
@@ -1,19 +1,34 @@
 /// <reference path="typings/index.d.ts" />
 class Webcam {
     static IMAGE_CONSTRAINTS:  MediaStreamConstraints = {video: true, audio: false};
-    static VIDEO_CONSTRAINTS:  MediaStreamConstraints = {video: true, audio: true};
+    static VIDEO_CONSTRAINTS:  MediaStreamConstraints = {
+        video: {
+            width: 240,
+            height: 160
+        }
+    };
+
+    private video: any = null;
 
     constructor(private tagId: string) {
+        this.video = document.querySelector(tagId);
+    }
+    
+    capture() {
+        let canvas = document.createElement("canvas");
+        let ctx = canvas.getContext("2d");
+        ctx.drawImage(this.video, 0, 0);
+        return ctx.getImageData(0, 0, 240, 160);
     }
 
     requestVideo() {
         navigator.mediaDevices.getUserMedia(Webcam.VIDEO_CONSTRAINTS).
-            then(this.handleVideo).
-            catch(this.handleVideoError);
+            then(this.handleVideo.bind(this)).
+            catch(this.handleVideoError.bind(this));
     }
 
     private handleVideo(stream: MediaStream) {
-        console.log(stream);
+        this.video.srcObject = stream;
     }
 
     private handleVideoError(error: MediaStreamError) {
@@ -21,5 +36,5 @@ class Webcam {
     }
 }
 
-var webcam: Webcam = new Webcam("vidInput");
+var webcam: Webcam = new Webcam("#webcam");
 webcam.requestVideo();
